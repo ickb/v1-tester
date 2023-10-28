e5ab7d06033b8cce0a50b9119788b5ce3de07828
phroi <90913182+phroi@users.noreply.github.com>
2023-10-28 14:25:52 +0000

    Add Account utils

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,30 @@
-import { key } from "@ckb-lumos/hd";
 import {
-    TransactionBuilder, defaultScript, fund, initializeChainAdapter, secp256k1SignerFrom
+    TransactionBuilder, fund, initializeChainAdapter, secp256k1SignerFrom
 } from "lumos-utils";
 
-import { newLimitOrderUtils, ickbSudtScript } from "v1-core";
+import { newLimitOrderUtils } from "v1-core";
 
 import config from "./config.json";
 import { Config } from "@ckb-lumos/config-manager";
 import { BI, parseUnit } from "@ckb-lumos/bi";
-import { Script } from "@ckb-lumos/base";
+import { Account, randomSecp256k1Account } from "./account";
 
 async function main() {
-
     await initializeChainAdapter("devnet", config as Config);
     const { create, sudtHash } = newLimitOrderUtils();
 
-    const privKey = "0xd00c06bfd800d27397002dca6fb0993d5ba6399b4238b2f29ee9deb97593d2bc";
-    const pubKey = key.privateToPublic(privKey);
-    const accountLock: Script = {
-        ...defaultScript("SECP256K1_BLAKE160"),
-        args: key.publicKeyToBlake160(pubKey)
-    };
+    //Genesis account
+    const genesisAccount = randomSecp256k1Account("0xd00c06bfd800d27397002dca6fb0993d5ba6399b4238b2f29ee9deb97593d2bc");
 
-    const { txHash } = await (await fund(new TransactionBuilder(accountLock, secp256k1SignerFrom(privKey))
+    const newTransactionBuilder = (account: Account) => new TransactionBuilder(
+        account.lockScript,
+        secp256k1SignerFrom(account.privKey)
+    );
+
+    const { txHash } = await (await fund(newTransactionBuilder(genesisAccount)
         .add("output", "end", create({
             ckbAmount: parseUnit("1000", "ckb"),
-            terminalLock: accountLock,
+            terminalLock: genesisAccount.lockScript,
             sudtHash,
             isSudtToCkb: false,
             ckbMultiplier: BI.from(1),
