9fac317f2cb26bf1a03c0bf849c88469a4d96127
phroi <90913182+phroi@users.noreply.github.com>
2023-10-31 12:00:12 +0000

    Add sudt to ckb limit order

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import {
-    TransactionBuilder, fund, initializeChainAdapter, secp256k1SignerFrom
+    initializeChainAdapter, secp256k1SignerFrom
 } from "lumos-utils";
 
-import { newLimitOrderUtils } from "v1-core";
+import { IckbTransactionBuilder, fund, newLimitOrderUtils } from "v1-core";
 
 import config from "./config.json";
 import { Config } from "@ckb-lumos/config-manager";
@@ -10,20 +10,26 @@ import { BI, parseUnit } from "@ckb-lumos/bi";
 import { Account, randomSecp256k1Account } from "./account";
 
 async function main() {
+    const args = process.argv.slice(2)
+    if (args.length != 1 || (args[0] !== "SUDT2CKB" && args[0] !== "CKB2SUDT")) {
+        throw Error("Invalid command line arguments " + args.join(" "));
+    }
+
     await initializeChainAdapter("devnet", config as Config);
     const { create, sudtHash } = newLimitOrderUtils();
 
     //Genesis account
     const genesisAccount = randomSecp256k1Account("0xd00c06bfd800d27397002dca6fb0993d5ba6399b4238b2f29ee9deb97593d2bc");
 
-    const newTransactionBuilder = (account: Account) => new TransactionBuilder(
+    const newTransactionBuilder = (account: Account) => new IckbTransactionBuilder(
         account.lockScript,
         secp256k1SignerFrom(account.privKey)
     );
 
     const { txHash } = await (await fund(newTransactionBuilder(genesisAccount)
         .add("output", "end", create({
-            ckbAmount: parseUnit("1000", "ckb"),
+            ckbAmount: args[0] == "CKB2SUDT" ? parseUnit("50000", "ckb") : undefined,
+            sudtAmount: args[0] == "SUDT2CKB" ? parseUnit("50000", "ckb") : undefined,
             terminalLock: genesisAccount.lockScript,
             sudtHash,
             isSudtToCkb: false,
