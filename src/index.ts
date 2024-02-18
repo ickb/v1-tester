8331deea69a60e2a4f6e4cd16d980ddce28449f2
phroi <90913182+phroi@users.noreply.github.com>
2024-02-18 13:12:57 +0000

    Cancel old unmatched CKB to SUDT orders

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ async function main() {
     if (capacities.length === 0 && sudts.length === 0) {
         console.log("Funding limit order creator");
         const genesisAccount = secp256k1Blake160(genesisDevnetKey);
-        const txHash = await genesisAccount.transfer(account.lockScript, parseUnit("1000000", "ckb"));
+        const txHash = await genesisAccount.transfer(account.lockScript, parseUnit("10000000", "ckb"));
         console.log(txHash);
         return;
     }
@@ -48,37 +48,72 @@ async function main() {
     assets = ickbSudtFundAdapter(assets, account.lockScript, sudts);
     assets = limitOrderFundAdapter(assets, ckb2SudtOrders, sudt2ckbOrders);
 
-    console.log("CKB :", assets["CKB"].balance.div(100000000).toString());
-    console.log("ICKB:", assets["ICKB_SUDT"].balance.div(100000000).toString());
+    console.log(
+        "CKB :",
+        assets["CKB"].availableBalance.div(100000000).toString(),
+        "+",
+        assets["CKB"].balance.sub(assets["CKB"].availableBalance).div(100000000).toString()
+    );
 
-    const { ckbMultiplier, sudtMultiplier } = ickbExchangeRatio(tipHeader);
+    console.log(
+        "ICKB:",
+        assets["ICKB_SUDT"].availableBalance.div(100000000).toString(),
+        "+",
+        assets["ICKB_SUDT"].balance.sub(assets["ICKB_SUDT"].availableBalance).div(100000000).toString()
+    );
 
     const ickbEquivalentBalance = ckb2Ickb(assets["CKB"].balance, tipHeader).toNumber();
     const ickbBalance = assets["ICKB_SUDT"].balance.toNumber();
 
     const r0 = Math.random();
     const isSudtToCkb = Math.round((ickbEquivalentBalance + ickbBalance) * r0) > ickbEquivalentBalance;
-    console.log(isSudtToCkb ? "SUDT -> CKB" : "CKB -> SUDT");
+    // const isSudtToCkb = false;
+    // const isSudtToCkb = true;
 
-    const r1 = Math.random();
+    const r1 = 4 * Math.random();
     const ckbAmount = !isSudtToCkb ? BI.from(Math.round(r1 * ckbSoftCapPerDeposit(tipHeader).toNumber())) : undefined;
     const sudtAmount = isSudtToCkb ? BI.from(Math.round(r1 * ICKB_SOFT_CAP_PER_DEPOSIT.toNumber())) : undefined;
 
     let tx = TransactionSkeleton();
-    tx = limitOrderInfo.create(tx, {
-        ckbAmount,
-        sudtAmount,
-        terminalLock: account.lockScript,
-        sudtHash: limitOrderInfo.sudtHash,
-        isSudtToCkb,
-        ckbMultiplier,
-        sudtMultiplier,
-    });
 
-    tx = fund(tx, assets);
+    //Cancel CKB -> SUDT orders older than 1000 blocks
+    for (const o of ckb2SudtOrders) {
+        if (BI.from(o.cell.blockNumber).add(1000).lt(tipHeader.number)) {
+            console.log("Cancelling old CKB -> SUDT order");
+            tx = limitOrderInfo.cancel(tx, o, false);
+        } else {
+            break;
+        }
+    }
 
-    const txHash = await sendTransaction(account.signer(tx));
-    console.log(txHash);
+    try {
+        let { ckbMultiplier, sudtMultiplier } = ickbExchangeRatio(tipHeader);
+        if (isSudtToCkb) {//Pay 0.1% fee to bot
+            sudtMultiplier = sudtMultiplier.sub(sudtMultiplier.div(1000));
+        } else {
+            sudtMultiplier = sudtMultiplier.add(sudtMultiplier.div(1000));
+        }
+
+        const txWithNewOrder = limitOrderInfo.create(tx, {
+            ckbAmount,
+            sudtAmount,
+            terminalLock: account.lockScript,
+            sudtHash: limitOrderInfo.sudtHash,
+            isSudtToCkb,
+            ckbMultiplier,
+            sudtMultiplier,
+        });
+        tx = fund(txWithNewOrder, assets, true);
+        console.log(isSudtToCkb ? "SUDT -> CKB" : "CKB -> SUDT");
+        const txHash = await sendTransaction(account.signer(tx));
+        console.log(txHash);
+    } catch (e: any) {
+        if (tx.outputs.size > 0) {
+            tx = fund(tx, assets, true);
+            const txHash = await sendTransaction(account.signer(tx));
+            console.log(txHash);
+        }
+    }
 }
 
 async function siftCells(
@@ -99,7 +134,7 @@ async function siftCells(
     ])).flat();
 
     const { capacities, sudts, notSimples } = simpleSifter(cells, limitOrderInfo.sudtType, account.expander);
-    const { ckb2SudtOrders, sudt2ckbOrders } = limitOrderInfo.sifter(notSimples, account.lockScript);
+    const { ckb2SudtOrders, sudt2ckbOrders } = limitOrderInfo.sifter(notSimples, account.lockScript, "desc");
 
     return {
         capacities, sudts,
